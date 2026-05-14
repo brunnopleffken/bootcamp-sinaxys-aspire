b1c11bcdfb586697d8a01039613f47adef082467
Brunno Pleffken Hosti <brunno.pleffken@outlook.com>
2026-05-14 18:27:13 -0300

    feat: add HttpClientFactory

--- Katalog.Web/Program.cs
+++ Katalog.Web/Program.cs
@@ -12,11 +12,10 @@ builder.Services.AddRazorComponents()
 
 builder.Services.AddOutputCache();
 
-builder.Services.AddHttpClient<WeatherApiClient>(client =>
+builder.Services.AddHttpClient("KatalogAPI", client =>
 {
-    // This URL uses "https+http://" to indicate HTTPS is preferred over HTTP.
-    // Learn more about service discovery scheme resolution at https://aka.ms/dotnet/sdschemes.
-    client.BaseAddress = new("https+http://apiservice");
+    client.BaseAddress = new Uri("http://localhost:5545");
+    client.Timeout = TimeSpan.FromSeconds(10);
 });
 
 var app = builder.Build();
